617808397ff6172170ab0539cdb29d9927479506
jamesvrooney <jamesvrooney@hotmail.com>
2022-01-02 06:59:13 +0000

    Added Open Feign.

--- customer/src/main/java/com/jamesvrooney/customer/services/CustomerServiceImpl.java
+++ customer/src/main/java/com/jamesvrooney/customer/services/CustomerServiceImpl.java
@@ -1,13 +1,13 @@
 package com.jamesvrooney.customer.services;
 
+import com.jamesvrooney.clients.fraud.FraudClient;
+import com.jamesvrooney.clients.model.FraudCheckResponse;
 import com.jamesvrooney.customer.model.Customer;
 import com.jamesvrooney.customer.model.CustomerRegistrationRequest;
-import com.jamesvrooney.customer.model.FraudCheckResponse;
 import com.jamesvrooney.customer.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
-    private final RestTemplate restTemplate;
+    private final FraudClient fraudClient;
 
     @Override
     public Customer registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
@@ -29,11 +29,7 @@ public class CustomerServiceImpl implements CustomerService {
 
         Customer savedCustomer = customerRepository.save(customer);
 
-        final FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                savedCustomer.getId()
-        );
+        final FraudCheckResponse fraudCheckResponse = fraudClient.isFraudster(savedCustomer.getId());
 
         if (fraudCheckResponse.isFraudster()) {
             throw new IllegalStateException("Customer is a fraudster");
